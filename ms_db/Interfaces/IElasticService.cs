fd62bc50337d683ce6c4dd1486a6d82f78254264
zzfima <zzfima@gmail.com>
2023-10-11 15:16:21 +0300

    implement elastic service (except of search)

--- ms_db/Interfaces/IElasticService.cs
+++ ms_db/Interfaces/IElasticService.cs
@@ -1,16 +1,72 @@
 using shared_library.Models;
 
-namespace ms_db.Interfaces;
-
+/// <summary>
+/// Elastic client contracts
+/// </summary>
 public interface IElasticService
 {
-    public CompletedText Create(CompletedText completedText);
+    /// <summary>
+    /// Create Index
+    /// </summary>
+    /// <param name="completedText">text to index</param>
+    public void Create(CompletedText completedText);
 
+    /// <summary>
+    /// Create Index Async
+    /// </summary>
+    /// <param name="completedText">text to index</param>
+    /// <returns></returns>
+    public Task CreateAsync(CompletedText completedText);
+
+    /// <summary>
+    /// Read text
+    /// </summary>
+    /// <param name="id">id</param>
+    /// <returns>text</returns>
     public CompletedText Read(string id);
 
-    public CompletedText Update(CompletedText completedText);
+    /// <summary>
+    /// Read text Async
+    /// </summary>
+    /// <param name="id">id</param>
+    /// <returns>text</returns>
+    public Task<CompletedText> ReadAsync(string id);
 
+    /// <summary>
+    /// Update text
+    /// </summary>
+    /// <param name="completedText">updated text</param>
+    public void Update(CompletedText completedText);
+
+    /// <summary>
+    /// Update text Async
+    /// </summary>
+    /// <param name="completedText">updated text</param>
+    public Task UpdateAsync(CompletedText completedText);
+
+    /// <summary>
+    /// Delete text
+    /// </summary>
+    /// <param name="id">text id to be deleted</param>
     public void Delete(string id);
 
-    public List<CompletedText> Serach(List<string> keywords);
+    /// <summary>
+    /// Delete text Async
+    /// </summary>
+    /// <param name="id">text id to be deleted</param>
+    public Task DeleteAsync(string id);
+
+    /// <summary>
+    /// Search text by keywords
+    /// </summary>
+    /// <param name="keywords">keywords for search</param>
+    /// <returns></returns>
+    public List<CompletedText> Search(List<string> keywords);
+
+    /// <summary>
+    /// Search text by keywords Async
+    /// </summary>
+    /// <param name="keywords">keywords for search</param>
+    /// <returns></returns>
+    public Task<List<CompletedText>> SearchAsync(List<string> keywords);
 }
--- ms_db/Services/ElasticService.cs
+++ ms_db/Services/ElasticService.cs
@@ -1,32 +1,126 @@
-using ms_db.Interfaces;
+using Elasticsearch.Net;
+using Nest;
 using shared_library.Models;
 
-namespace ms_db.Services;
-
+/// <summary>
+/// Implementation Elastic client
+/// </summary>
 public class ElasticService : IElasticService
 {
-    public CompletedText Create(CompletedText completedText)
+    #region fields
+    private readonly ElasticClient _elasticClient;
+    private readonly string _elasticIndexName;
+    #endregion
+
+    #region ctor
+    public ElasticService(string elasticHostName, string elasticIndexName)
     {
-        throw new NotImplementedException();
+        _elasticIndexName = elasticIndexName;
+
+        var nodes = new Uri[] { new Uri(elasticHostName), };
+        var connectionPool = new StaticConnectionPool(nodes);
+        var connectionSettings = new ConnectionSettings(connectionPool).DisableDirectStreaming();
+        _elasticClient = new ElasticClient(connectionSettings.DefaultIndex(_elasticIndexName));
+
+        CreateIndexIfNotExists();
+    }
+    #endregion
+
+    private void CreateIndexIfNotExists()
+    {
+        var indexExists = _elasticClient.Indices.Exists(_elasticIndexName);
+        if (!indexExists.Exists)
+        {
+            var response = _elasticClient.Indices.Create(_elasticIndexName,
+               index => index.Map<CompletedText>(
+                   x => x.AutoMap()));
+        }
     }
 
+    #region Create
+    public void Create(CompletedText completedText)
+    {
+        IndexResponse indexResponse = _elasticClient.IndexDocument(completedText);
+        var result = indexResponse.Result;
+        if (result != Result.Created && result != Result.Updated)
+            throw new Exception($"Create failed. Result: {result}");
+    }
+
+    public async Task CreateAsync(CompletedText completedText)
+    {
+        IndexResponse indexResponse = await _elasticClient.IndexDocumentAsync(completedText);
+        var result = indexResponse.Result;
+        if (result != Result.Created && result != Result.Updated)
+            throw new Exception($"Create failed. Result: {result}");
+    }
+    #endregion
+
+    #region Read
     public CompletedText Read(string id)
     {
-        throw new NotImplementedException();
-    }
- 
-    public CompletedText Update(CompletedText completedText)
-    {
-        throw new NotImplementedException();
+        var searchResponse = _elasticClient.Search<CompletedText>(ct => ct
+        .From(0)
+        .Size(1)
+        .Query(q => q
+            .Match(m => m
+                .Field(f => f.Id)
+                .Query(id))));
+
+        var completedText = searchResponse.Documents.First();
+        return completedText;
     }
 
+    public async Task<CompletedText> ReadAsync(string id)
+    {
+        var gg = Read(id);
+        var searchResponse = await _elasticClient.SearchAsync<CompletedText>(q => q
+        .Index(_elasticIndexName)
+        .Query(qs => qs
+            .Match(m => m
+                .Field(f => f.Id)
+                .Query(id))));
+
+        var completedText = searchResponse.Documents.First();
+        return completedText;
+    }
+    #endregion
+
+    #region Update
+    public void Update(CompletedText completedText) => Create(completedText);
+    public async Task UpdateAsync(CompletedText completedText) => await CreateAsync(completedText);
+    #endregion
+
+    #region Delete
     public void Delete(string id)
     {
-        throw new NotImplementedException();
+        var response = _elasticClient.DeleteByQuery<CompletedText>(q => q
+        .Index(_elasticIndexName)
+        .Query(qs => qs
+            .Match(m => m
+                .Field(f => f.Id)
+                .Query(id))));
     }
 
-    public List<CompletedText> Serach(List<string> keywords)
+    public async Task DeleteAsync(string id)
+    {
+        var response = await _elasticClient.DeleteByQueryAsync<CompletedText>(q => q
+        .Index(_elasticIndexName)
+        .Query(qs => qs
+            .Match(m => m
+                .Field(f => f.Id)
+                .Query(id))));
+    }
+    #endregion
+
+    #region Search
+    public List<CompletedText> Search(List<string> keywords)
     {
         throw new NotImplementedException();
     }
+
+    public async Task<List<CompletedText>> SearchAsync(List<string> keywords)
+    {
+        throw new NotImplementedException();
+    }
+    #endregion
 }
